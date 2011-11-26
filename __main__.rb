acf2685374addfe7c041ce24fb47534d62711a17
David Cohen <dcohen.consulting@gmail.com>
2011-11-26 11:10:08 -0800

    Prototype is almost working.  Need to work out location changes using strings

---
+++ __main__.rb
@@ -0,0 +1,53 @@
+require_relative "Player"
+require_relative "Location"
+
+#eval File.read('stuff_to_eval.rb')
+##################
+##TODO: Testing
+##################
+locations = {"testlocation" => Location.new(
+        description="you are standing in a test location, looking around at the blank white nothingness.",
+        choices = {
+          "go to vienna" => Choice.new(
+                choice_description="hop on a plane and go to vienna",
+                location_exit="locvienna",
+                making_choice_description="You run to the nearest airport, buy a ticket, and get on a plane to Vienna."
+          )
+        }
+
+),
+
+"vienna" => Location.new(
+        description="you are surrounded by an old, beautiful city.  Yay!",
+        choices = {
+          "go to testlocation" => Choice.new(
+                choice_description="swing back to the nothingness",
+                location_exit="testlocation",
+                making_choice_description="You suddenly cease to exist in the normal world."
+          )
+        }
+
+),
+
+# end locations dict
+}
+
+# instantiate player global
+Player1 = Player.new
+
+#TODO: Debug -- make this a string, and make sure string input can lookup the correct location to go to next.
+# looks like change_location will have to take a string as input and do the lookup to see which loc has that name
+Player1.change_location("testlocation")
+
+# give the description from the player's currentlocation
+Player1.currentlocation.tell_story
+Player1.currentlocation.list_choices
+
+# take input
+#TODO: implement the get_player_input function
+print "What's your choice, yo?"
+playerinput = gets().chomp!
+
+
+# execute the player's choice
+Player1.currentlocation.choose_option(playerinput)
