4a41346850aaa79169d3424adb146fb248e5e189
David Cohen <dcohen.consulting@gmail.com>
2011-11-26 11:35:31 -0800

    split locations and choices out into a separate file that is evaluated from __main__

--- adventuregame/__main__.rb
+++ adventuregame/__main__.rb
@@ -1,36 +1,8 @@
 require_relative "Player"
 require_relative "Location"
 
-#eval File.read('stuff_to_eval.rb')
-##################
-##TODO: Testing
-##################
-Locations = {"testlocation" => Location.new(
-        description="you are standing in a test location, looking around at the blank white nothingness.",
-        choices = {
-          "go to vienna" => Choice.new(
-                choice_description="hop on a plane and go to vienna",
-                location_exit="vienna",
-                making_choice_description="You run to the nearest airport, buy a ticket, and get on a plane to Vienna."
-          )
-        }
+eval File.read('locations.rb')
 
-),
-
-"vienna" => Location.new(
-        description="you are surrounded by an old, beautiful city.  Yay!",
-        choices = {
-          "go to testlocation" => Choice.new(
-                choice_description="swing back to the nothingness",
-                location_exit="testlocation",
-                making_choice_description="You suddenly cease to exist in the normal world."
-          )
-        }
-
-),
-
-# end locations dict
-}
 
 # instantiate player global
 Player1 = Player.new
---
+++ adventuregame/locations.rb
@@ -0,0 +1,29 @@
+##################
+##TODO: Testing
+##################
+Locations = {"testlocation" => Location.new(
+        description="you are standing in a test location, looking around at the blank white nothingness.",
+        choices = {
+          "go to vienna" => Choice.new(
+                choice_description="hop on a plane and go to vienna",
+                location_exit="vienna",
+                making_choice_description="You run to the nearest airport, buy a ticket, and get on a plane to Vienna."
+          )
+        }
+
+),
+
+"vienna" => Location.new(
+        description="you are surrounded by an old, beautiful city.  Yay!",
+        choices = {
+          "go to testlocation" => Choice.new(
+                choice_description="swing back to the nothingness",
+                location_exit="testlocation",
+                making_choice_description="You suddenly cease to exist in the normal world."
+          )
+        }
+
+),
+
+# end locations dict
+}
